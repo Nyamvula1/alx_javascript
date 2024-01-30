510750feb149aec3f6a4f090dd3c89230e95d99e
Khadija <khadijabeja@gmail.com>
2024-01-30 16:06:10 +0300

    4 JS

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,7 +1,13 @@
-function add(a, b) {
-    return a + b;
-} 
+// function add(a, b) {
+//     return a + b;
+// } 
+// let num1 = 3;
+// let num2 = 5;
+// let sum = add(num1, num2);
+// console.log(sum);
+let addition = (a, b) => a + b;
+ 
 let num1 = 3;
 let num2 = 5;
-let sum = add(num1, num2);
-console.log(sum);
+let sum = addition(num1, num2);
+console.log( sum);
