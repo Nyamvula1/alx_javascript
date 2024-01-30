9467bcaf08ae974d1198872bad7d66abd4e6fa56
Khadija <khadijabeja@gmail.com>
2024-01-30 15:58:22 +0300

    4 JS

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,6 +1,13 @@
-function add(a, b) {
+// function add(a, b) {
+//     return a + b;
+// } 
+// let a = 3;
+// let b = 5;
+// console.log(add(a, b));
+let add = function(a, b) {
     return a + b;
-} 
-let a = 3;
-let b = 5;
-console.log(add(a, b));
+};
+
+// Example usage:
+let result = add(3, 5);
+console.log(result);
