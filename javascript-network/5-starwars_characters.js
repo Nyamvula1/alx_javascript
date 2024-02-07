22bf0a483682e6002a63f6587e58336791d40823
Khadija <khadijabeja@gmail.com>
2024-02-07 23:11:10 +0300

    starwars characters

--- javascript-network/5-starwars_characters.js
+++ javascript-network/5-starwars_characters.js
@@ -1,24 +1,44 @@
-#!/usr/bin/node
+// #!/usr/bin/node
 
+// const request = require('request');
+
+// const movieId = process.argv[2];
+// const apiUrl = `https://swapi.co/api/films/${movieId}/`;
+
+// request(apiUrl, (error, response, body) => {
+//   if (!error && response.statusCode === 200) {
+//     const movie = JSON.parse(body);
+//     console.log(`Characters of Star Wars Episode ${movieId}: ${movie.title}`);
+//     console.log('---------------------------');
+//     movie.characters.forEach(characterUrl => {
+//       request(characterUrl, (error, response, body) => {
+//         if (!error && response.statusCode === 200) {
+//           const character = JSON.parse(body);
+//           console.log(character.name);
+//         }
+//       });
+//     });
+//   } else {
+//     console.error('Error:', error || response.statusCode);
+//   }
+// });
 const request = require('request');
 
-const movieId = process.argv[2];
-const apiUrl = `https://swapi.co/api/films/${movieId}/`;
+const Id = process.argv[2];//movie to be passed in the terminal
+const url = `https://swapi.dev/api/films/${Id}/`;
 
-request(apiUrl, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    const movie = JSON.parse(body);
-    console.log(`Characters of Star Wars Episode ${movieId}: ${movie.title}`);
-    console.log('---------------------------');
-    movie.characters.forEach(characterUrl => {
-      request(characterUrl, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-          const character = JSON.parse(body);
-          console.log(character.name);
-        }
-      });
-    });
-  } else {
-    console.error('Error:', error || response.statusCode);
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
   }
-});
+
+  const data = JSON.parse(body);
+  const characters = data.characters;
+  for (const character of characters) {
+    request(character, (error, response, body) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      const
