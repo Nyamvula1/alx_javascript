18e20f52f3715b9566a5247a960bc61858ee46ef
Khadija <khadijabeja@gmail.com>
2024-02-07 20:06:19 +0300

    completed tasks

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,6 +1,7 @@
-#!/usr/bin/node
-const request = require('request');
-request(process.argv[2], function (error, response, body) {
+#!/usr/bin/node //tells the operating system that this script should be executed using the Node.js interpreter.
+const request = require('request');//This line imports the request module, which allows us to make HTTP requests.
+request(process.argv[2], function (error, response, body) {//This line makes a GET request to the URL provided as the first command-line argument (process.argv[2]). When the request completes, it executes a 
+    //callback function with three arguments: error, response, and body.
   if (!error) {
     const results = JSON.parse(body).results;
     console.log(results.reduce((count, movie) => {
--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
+class Rectangle {
     constructor (w, h) {
       if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
-    }
+    };
   
     print () {
-      for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+      const chr = 'X';
+      for (let i = 0; i < this.height; i++) {
+      console.log(chr.repeat(this.width));
     }
-  };
+  }
+};
+module.exports = Rectangle;
